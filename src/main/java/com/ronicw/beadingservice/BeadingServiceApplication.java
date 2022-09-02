ba3e92dc6f58f0deb09fe28310e14ad2a9c24e9a
Radhika A Woodruff <ronicw@hotmail.com>
2022-09-01 19:14:32 -0500

    open #1 https://start.spring.io/ - use sprint initializr at this url to generate the necessary boiler plate code for the backend service. unzipped the created file at this url and copied the files into the project. check the dependencies in the build.gradle file to see the dependencies picked.

---
+++ src/main/java/com/ronicw/beadingservice/BeadingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ronicw.beadingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeadingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeadingServiceApplication.class, args);
+	}
+
+}
